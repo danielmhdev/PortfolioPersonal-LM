751ee4ee70d9a5b2e74ec175e48dd7d9f7ba0cfd
Daniel <danaelal@gmail.com>
2025-07-23 19:07:03 +0200

    responsive moviles

--- js/javascript.js
+++ js/javascript.js
@@ -15,3 +15,17 @@ function despliegaMenu() {
 }
 
 nav();
+
+//COOKIES
+
+const popup = document.getElementById("cookiePopup");
+const btn = document.getElementById("acceptCookies");
+
+if (!localStorage.cookiesAccepted){
+    popup.classList.add("show");
+}
+
+btn.addEventListener("click",function(){
+    localStorage.cookiesAccepted = "true";
+    popup.classList.remove("show");
+});
